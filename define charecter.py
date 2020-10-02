5f60314c0eff981390202c65d902b9ba911de87c
code-Dev-2001 <debabrtagiri5525@gmail.com>
2020-10-02 16:02:20 +0530

    Frist commit

---
+++ define charecter.py
@@ -0,0 +1,28 @@
+'''
+SO CNA U KNOW YOUR partner_charector for  IN MY PROGRAM
+try'it
+
+****************************************************
+
+        *****        *****
+      *       *   *       *
+     *          *         *
+       *                 *
+        *             *
+           *       *
+                *
+
+'''
+import random
+
+partner_charector=["Good","Bad","middle","very Bad"]
+
+player_name = input("Hellow ,what is your name?")
+
+print("ok so " + player_name + " I Hope you R so :")
+
+
+choice=random.choice(partner_charector)
+print("your partner is ",choice,"charector")
+
+
